568e4fa3aebc35380183141296ff6f0e6de4c97f
Taylor Romero <me@taylorrome.ro>
2022-12-19 11:06:30 -0600

    major: removed impl export (gotta slow down)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export { default as AuthorizerFactory } from './authorizer/AuthorizerFactory'
 export { default as buildPermissionContractId } from './utilities/buildPermissionContractId'
-export { default as AuthorizerImpl } from './authorizer/Authorizer'
